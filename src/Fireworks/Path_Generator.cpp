d49ca2231daf1dbe7c9efdf38a73dee213986ad5
Liuwsh <eastwind10274@163.com>
2023-11-19 03:42:34 +0800

    build: 删除冗余文件

--- src/Fireworks/Path_Generator.cpp
+++
@@ -1,9 +0,0 @@
-#include "Path_Generator.h"
-
-int PathGenerator::gravity(glm::vec3& pos, glm::vec3& v, uint64_t ms_passby, float g) {
-    float t = (float)ms_passby / 1000;
-    pos += v * t;
-    pos.y += g * t * t / 2;
-    v.y += g * t;
-    return 0;
-}
