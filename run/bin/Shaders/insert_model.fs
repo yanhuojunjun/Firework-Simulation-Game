6fc0d3fe5c0ec8cf648266f7342b36e9034421c9
Liuwsh <eastwind10274@163.com>
2023-12-03 16:17:31 +0800

    feat: 增加一个火箭模型及其着色器

---
+++ run/bin/Shaders/insert_model.fs
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_diffuse2;
+uniform sampler2D texture_specular1;
+
+void main()
+{    
+    FragColor = mix(texture(texture_diffuse1, TexCoords), texture(texture_diffuse2, TexCoords), 0.5);
+}
